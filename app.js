40edf62b180dfb4b4445a7edfe0edf883830a630
johnlindquist <johnlindquist@gmail.com>
2016-03-01 14:00:01 -0700

    adding relations

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const films = require('./films.json');
 const people = require('./people.json');
 const planets = require('./planets.json');
 const species = require('./species.json');
-const starships = require('./starships.json');
+const starships = require('./starships.json')
 const transport = require('./transport.json');
 const vehicles = require('./vehicles.json');
 
@@ -23,19 +23,80 @@ const db = {
  * Lifting the "fields" to the root of the object
  * Using the "pk" as the "id"
  */
-const formattedDb = Object.keys(db).reduce((acc, current)=>{    
-    acc[current] = db[current].map((item)=>{
-        return Object.assign(item.fields, {id:item.pk});
+var fdb = Object.keys(db).reduce((acc, current) => {
+    acc[current] = db[current].map((item) => {
+        return Object.assign(
+            item.fields,
+            { id: item.pk }
+        );
     })
-    
+
     return acc;
-}, {})
+}, {});
+
+/***
+ * Relation definitions to match them with swapi.co
+ */
+const relations = [
+    {   
+        name: "people",
+        relation:[
+            {alias: "vehicles", table: "vehicles", name: "pilots", type: Array},
+            {alias: "starships", table: "starships", name: "pilots", type: Array},
+            {alias: "films", table: "films", name: "characters", type: Array},
+        ]
+    },
+    {   
+        name: "planets",
+        relation:[
+            {alias: "residents", table: "people", name: "homeworld", type: Number},
+            {alias: "films", table: "films", name: "planets", type: Array}
+        ]
+    },
+    {   
+        name: "starships",
+        relation:[
+            {alias: "films", table: "films", name: "starships", type: Array}
+        ]
+    }
+];
+
+function addRelation(db, host, relations) {
+    return relations.reduce((acc, relation) => {
+        const alias = relation.alias;
+        const table = relation.table;
+        const name = relation.name;
+        if(relation.type === Number){
+            acc[alias] = db[table]
+                .filter(item => item[name] === host.id)
+                .map(item => item.id)
+                
+            return acc;                
+        }
+                
+        acc[alias] = db[table]
+            .filter(item => item[name].indexOf(host.id) > -1)
+            .map(item => item.id)
+
+        return acc;
+    }, {})
+}
+
+const relationDb = relations.reduce((acc, current)=>{
+    acc[current.name] = acc[current.name].map((item) => {
+        return Object.assign(
+            item,
+            addRelation(acc, item, current.relation)
+        )
+    });
+    return acc;
+}, fdb)
 
 
 const server = jsonServer.create()
 server.use(jsonServer.defaults())
 
-const router = jsonServer.router(formattedDb);
+const router = jsonServer.router(relationDb);
 server.use(router)
 
 server.listen(3000)
